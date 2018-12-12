a782b525eeede0e096af89dd7e91d88663ef62db
Victor Rodriguez <vrodriguez@ANSI.org>
2018-12-11 19:18:19 -0500

    clearning1

--- CustomFinishedFunctions/CREATE-CUSTOMFUNCTIONTEMPLATE/garb.ps1
+++
@@ -1,6 +0,0 @@
-$filename = '\garb.ps1'
-$pathcheck = ((Get-Item -Path ".\").FullName) + $filename
-$pathcheck
-if(-not(Test-Path -Path ($pathcheck) ))
-                {write-host "NO"}##END of if
-            
